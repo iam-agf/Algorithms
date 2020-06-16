15af7475fd6c0f733dde7e4c3d15bb9adef10275
coder.generico <agf0710@gmail.com>
2020-06-15 23:39:46 -0500

    Added comments.

--- 2_knuth_morris_pratt.py
+++ 2_knuth_morris_pratt.py
@@ -5,6 +5,11 @@ class algorithm():
 
     @staticmethod
     def compute_prefix(string1):
+        '''
+        The first part of this algorithm is to get the length each substring of
+        string1 shares between it prefix and suffix. This will help us to avoid
+        restarting in the final algorithm.
+        '''
         m=len(string1)
         prefix_coincidences=[0] # We define the first comparation as zero
 
@@ -15,8 +20,14 @@ class algorithm():
             prefix_coincidences.append(j+1 if string1[j]==string1[q] else j)
         return prefix_coincidences
 
+
     @staticmethod
     def KnuthMorrisPratt_match1(string1,pattern):
+        '''
+        Once you have the prefix-suffix table, you must see where the pattern 
+        matches with string1. Here once you get a match, the algorithm finishes.
+        '''
+
         n,m=len(string1),len(pattern)
         if m==0: return 0
 
@@ -34,8 +45,15 @@ class algorithm():
             else: j+=1
         return -1
 
+
     @staticmethod
     def KnuthMorrisPratt(string1,pattern):
+        '''
+        In this case, changing the return in the second if of the while loop and
+        adding a redefining of the k variable, we can get the rest of matches 
+        the string1 has with the pattern.
+        '''
+
         n,m=len(string1),len(pattern) 
         if m==0: return 0 # If the len of pattern is 0, we end
 
