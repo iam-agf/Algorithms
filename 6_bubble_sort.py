a9b27fbe0bb47b1e318039e29ce9e6732c5ad179
coder.generico <agf0710@gmail.com>
2020-06-21 13:55:39 -0500

    Code corrections and explanation.

--- 6_bubble_sort.py
+++ 6_bubble_sort.py
@@ -1,16 +1,20 @@
 from dataclasses import dataclass
 
+'''
+The point in each iteration is to get the biggest element and get it to the top
+of the array. This iteration will be done n times.
+'''
 @dataclass
 class algorithm():
 
     @staticmethod
-    def insertion_sort(l):
+    def bubble_sort(l):
         for i in range(len(l)):
-            for j in range(i,0,-1):
+            for j in range(1,len(l)-i):
                 if l[j]<l[j-1]:
                     l[j-1],l[j]=l[j],l[j-1]
                 print(l)
         return l
 
-L=[1,5,3,0,2,9,4,7,6,8]
-print(algorithm.insertion_sort(L))
+L=[8,5,3,0,2,9,4,7,6,1]
+print(algorithm.bubble_sort(L))
