df9a3168854de33fac707d64c3c28b657c26d771
coder.generico <agf0710@gmail.com>
2020-06-22 01:14:32 -0500

    Added a limit for surpassing indices.

--- 4_binary_search.py
+++ 4_binary_search.py
@@ -7,19 +7,17 @@ class algorithm():
     def binarySearch(checkingList,param):
         not_found=True
         step=0
-        
         left_lim,right_lim=0,len(checkingList)
         
         while not_found:
             step+=1
             pointer=(left_lim+right_lim)//2
-        
+            if pointer>len(checkingList)-1: return -1 
             if checkingList[pointer]>param: right_lim=pointer-1
             elif checkingList[pointer]<param: left_lim=pointer+1
             else: return pointer,step 
 
             print(left_lim,right_lim)
-
         return "didn't found the value {}".format(param)
         
 print(algorithm.binarySearch(list(range(1000000)),2020))
