41139c52122f0e2e71727ac055c1265e9c7eff24
agf-nohchil <61362029+iam-agf@users.noreply.github.com>
2021-02-15 11:27:05 +0100

    Corrections for better understanding
    Instead of a class, just made it a function. It's unnecesary the class-styled structure

--- 01_rabin_karp.py
+++ 01_rabin_karp.py
@@ -1,29 +1,31 @@
-from dataclasses import dataclass
+def RabinKarp(pattern, string, repetitions = False):
+    '''
+    Complexity time O(n)
 
-'''
-The main idea of the algorithm is to hash each substring with the same size that
-has pattern and check if both have the same value. If it finds it, it ends.
-'''
+    The function will return the starting index where the 
+    string matches and where it finishes. It's case sensitive.
 
-@dataclass
-class algorithm():
+    The main idea of the algorithm is to compare the hash of 
+    each substring with the same size that has pattern if both 
+    have the same value. If it finds it, it ends.
 
-    @staticmethod
-    def RabinKarp(pattern,string):
-        '''
-        Complexity time O(n)
-        
-        The function will return the starting index where the string matches
-        and the final index where it finishes. It won't detect lower or upper
-        cases.
-        '''
+    If you need the repetitions (repetitions = True), the 
+    algorithm can save the indices when they substring matches 
+    and continue checking the rest of the main string. In the end
+    it will return a list of tuples (start, end) that define the
+    positions for the substring that matches.
+    '''
 
-        bs_len=len(string)
-        p_len=len(pattern)
+    string_len = len(string)
+    pattern_len = len(pattern)
 
-        for i in range(bs_len-p_len+1):
-            if hash(pattern) ==hash(string[i:i+p_len]):
-                return(i,i+p_len-1) # i+p_len-1 is the last index that matches.
-
-
-print(algorithm.RabinKarp("ame","and his name is John Cena"))
+    if repetitions == True:
+        indices = []
+    for i in range(string_len-pattern_len+1):
+        if hash(pattern) ==hash(string[i:i+pattern_len]):
+            if repetitions == False:
+                return(i,i+pattern_len-1) # i+p_len-1 is the last index that matches.
+            else:
+                indices.append((i,i+pattern_len-1))
+    return indices
+print(RabinKarp("ame","and his name is John Cena"))
